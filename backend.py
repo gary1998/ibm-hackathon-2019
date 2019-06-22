be89b4d17200ecf1872f3017b39f9386ab2cf8a8
Gaurav Goswami <gouravgoswami48@gmail.om>
2019-06-22 17:19:01 +0530

    Initial commit

---
+++ backend.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from ibm_watson import PersonalityInsightsV3
+import json
+import tweepy
+
+app = Flask(__name__)
+CORS(app)
+
+class DataStore():
+    api = None
+    friend=[]
+    follower=[]
+    userObj = None
+
+data = DataStore()
+
+@app.route('/')
+def hello():
+    return "Welcome to backend server - developed in Flask Python"
+
+@app.route('/twitterLogin')
+def loginFunc():
+    if 'consKey' and 'consSec' and 'accessTok' and 'accessSec' in request.args:
+        consumer_key = request.args['consKey']
+        consumer_secret = request.args['consSec']
+        access_token = request.args['accessTok']
+        access_token_secret = request.args['accessSec']
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        data.api = tweepy.API(auth)
+        data.userObj = data.api.me()
+        for user in tweepy.Cursor(data.api.friends, screen_name=data.userObj.screen_name).items():
+            data.friend.append(str(user.screen_name))
+        for user in tweepy.Cursor(data.api.followers, screen_name=data.userObj.screen_name).items():
+            data.follower.append(str(user.screen_name))    
+        return jsonify(name=data.userObj.name, followersNameList=data.follower, friendsNameList=data.friend)
+
+@app.route('/fetch')
+def fetchFunc():
+    if 'name' in request.args:
+        ffname = request.args['name']
+        tweetsData = ""
+        tweets = data.api.user_timeline(screen_name=ffname, count=100, tweet_mode='extended')
+        for info in tweets[:8]:
+            tweetsData += info.full_text
+        return jsonify(tweets=tweetsData)
+
+@app.route('/fetchMe')
+def fetchMyFunc():
+    myname = data.userObj.screen_name
+    tweetsData = ""
+    tweets = data.api.user_timeline(screen_name=myname, count=100, tweet_mode='extended')
+    for info in tweets[:8]:
+        tweetsData += info.full_text
+    return jsonify(tweets=tweetsData)
+
+@app.route('/analyze')
+def analyzeFunc():
+    if 'data' in request.args:
+        text = request.args['data']
+        personality_insights = PersonalityInsightsV3(
+            version='2017-10-13',
+            iam_apikey='uA6GpdKCXJyJCqJyhQEqwH9jSJxqlJhgYq7-uBBfPYL5',
+            url='https://gateway-lon.watsonplatform.net/personality-insights/api'
+        )
+        profile = personality_insights.profile(
+            text.encode('utf-8'),
+            'application/json',
+            consumption_preferences=True,
+            raw_scores=True
+        ).get_result()
+        return json.dumps(profile)
+    
+if __name__ == '__main__':
+    app.run()
