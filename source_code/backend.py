f856fb75b279d6d84fa6a15f642f933380dcabcf
Gaurav Goswami <gouravgoswami48@gmail.om>
2019-07-18 16:19:12 +0530

    docker images added

--- source_code/backend.py
+++ source_code/backend.py
@@ -85,4 +85,4 @@ def analyzeFunc():
         return jsonify(emotions=emotions, personality=personality)
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8000)
